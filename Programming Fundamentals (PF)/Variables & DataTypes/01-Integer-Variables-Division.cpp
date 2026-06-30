f49f0ed7e465412f8793f5750095397f5402c235
Ali-Irtza-Dev <ali.irtza.dev@gmail.com>
2026-06-30 22:18:27 +0500

    Variables and Data Types Q1

---
+++ Programming Fundamentals (PF)/Variables & DataTypes/01-Integer-Variables-Division.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+int a = 13;
+int b = 5;
+int c = a / b;
+cout<< " Value of a is = "<<a<<endl;
+cout<< " Value of b is = "<<b<<endl;
+cout<< " Value of c is = "<<c<<endl;
+}
