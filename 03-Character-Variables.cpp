c767363a5e751573294d5edb3c4f85c4a964e246
Ali-Irtza-Dev <ali.irtza.dev@gmail.com>
2026-06-30 22:23:41 +0500

    Q 2,3

---
+++ 03-Character-Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    char a = 'Z';
+    char b = '&';
+    char c = 'A';
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+}
---
+++ Programming Fundamentals (PF)/Variables & DataTypes/02-Float-Variables-Division.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+float a = 13.4;
+float b = 5.2;
+float c = a / b;
+cout<< " Value of a is = "<<a<<endl;
+cout<< " Value of b is = "<<b<<endl;
+cout<< " Value of c is = "<<c<<endl;
+}
