dc109f4a0bbab6459d2171fd200f8f8a94bcd69a
Ali-Irtza-Dev <ali.irtza.dev@gmail.com>
2026-07-14 23:15:41 +0500

    Q4

---
+++ Programming Fundamentals (PF)/Variables & DataTypes/04-Personal-Information.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int age ;
+ float height;
+ string name;
+ cout<<"Enter your age: ";
+ cin>>age;
+ cout<<"Enter your height: ";
+    cin>>height;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Your information:"<<endl;
+    cout<<age<<"\n"<<height<<"\n"<<name<<"\n"<<endl;
+}
