ec9e97b88d5f40f101426f8188d8f797264201b9
Ali-Irtza-Dev <ali.irtza.dev@gmail.com>
2026-07-14 23:47:36 +0500

    q4 changes and q5

--- Programming Fundamentals (PF)/Variables & DataTypes/04-Personal-Information.cpp
+++ Programming Fundamentals (PF)/Variables & DataTypes/04-Personal-Information.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+int main()
+{
  int age ;
  float height;
  string name;
+ bool residentofFSD;
+ int conditioncheck;
  cout<<"Enter your age: ";
  cin>>age;
  cout<<"Enter your height: ";
     cin>>height;
     cout<<"Enter your name: ";
     cin>>name;
+    cout<<"Are you a resident of FSD? (1 for Yes, 0 for No): ";
+    cin>>conditioncheck;
+
+    if (conditioncheck==1)
+    { residentofFSD=true;
+        cout<<"Person is a resident of FSD"<<endl;}
+    else
+    {
+        residentofFSD=false;
+     cout<<"Person is not a resident of FSD"<<endl;}
+
     cout<<"Your information:"<<endl;
-    cout<<age<<"\n"<<height<<"\n"<<name<<"\n"<<endl;
+    cout<<age<<"\n"<<height<<"\n"<<name<<endl;
 }
