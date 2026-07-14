00afb15c2fa0324d854430fcf8b9fc4eb9a1b7c4
Ali-Irtza-Dev <ali.irtza.dev@gmail.com>
2026-07-15 00:02:00 +0500

    q5

--- Programming Fundamentals (PF)/Variables & DataTypes/05-Student-Record.cpp
+++ Programming Fundamentals (PF)/Variables & DataTypes/05-Student-Record.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 int main(){
-int registrationNumber;
+string registrationNumber;
+string name;
+char grade;
+float gpa;
+bool scholarship_status;
 
+cout<<"Enter your registration number: ";
+cin>>registrationNumber;
+cout<<"Enter your name: ";
+cin>>name;
+cout<<"Enter your grade: ";
+cin>>grade;
+cout<<"Enter your GPA: ";
+cin>>gpa;
+if ( gpa>=3.5){
+    scholarship_status=true;
+    cout<<"Student is eligible for scholarship"<<endl;
+}
+else
+{
+    scholarship_status=false;
+    cout<<"Student is not eligible for scholarship"<<endl;
+}
+
+cout<<"Student Information:"<<endl;
+cout<<registrationNumber<<"\n"<<name<<"\n"<<grade<<"\n"<<gpa<<"\n"<<scholarship_status<<endl;
 }
