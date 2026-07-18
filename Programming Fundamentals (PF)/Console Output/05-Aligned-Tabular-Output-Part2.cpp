f6bc6de9348e3b32b4b16a9e9d8a577889af8aea
Ali-Irtza-Dev <ali.irtza.dev@gmail.com>
2026-07-18 08:43:40 +0500

    q5

---
+++ Programming Fundamentals (PF)/Console Output/05-Aligned-Tabular-Output-Part2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+   cout<<left;
+   /* width = minimum number of characters to use.
+*/
+cout<<setw(18) <<"Name"<<setw(10) <<"Registration"<<endl;
+cout<<setw(18) <<"--------"<<setw(10) << "--------"<<endl;
+cout<<setw(18) <<"Haris"<<setw(10) << "24-NTU-CS-1001"<<endl;
+cout<<setw(18) <<"Sana"<<setw(10) << "24-NTU-CS-1002"<<endl;
+cout<<setw(18) <<"Muhammad Ali"<<setw(10) << "24-NTU-CS-1003"<<endl;
+}
